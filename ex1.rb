7426d9fd3c9159fceda97bd312ed716c71041b06
Malarvizhi <malarvizhibe@gmail.com>
2017-08-17 15:58:07 +0530

    Exercise 1

---
+++ ex1.rb
@@ -0,0 +1,30 @@
+#Make your script print another line
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yah! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts 'This line "ends" here.'
+
+puts "\n"
+#Make your script print only one of the lines
+
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+puts "This is fun."
+#puts "Yah! Printing."
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+#puts 'This line "ends" here'
+
+puts "\n"
+
+#Quest 3
+
+puts "The symbol # 'Octothorpe' disables the particular line of code. That line won't be executed. So the symbol is used to disable a line or giving any comments"
+
+puts "\n"
