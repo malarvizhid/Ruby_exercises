64222fe7cc5b49f466d4371a893d670f86a2663f
malarvizhid <31063087+malarvizhid@users.noreply.github.com>
2017-08-21 18:24:10 +0530

    Odd, Even problem

---
+++ problem1.rb
@@ -0,0 +1,32 @@
+# Getting an integer input from user and testing whether it is numeric or not
+loop do
+	print "Enter your input : "
+	begin
+  		user_input = gets.chomp
+		user_input = Integer(user_input)
+	rescue
+  		print "Please enter an integer number:"
+  		retry
+	end
+
+	# Checking whether the number is odd
+	if user_input % 2 == 1
+		puts "Weird"
+	# If number is even checking other conditions
+	else 
+		if user_input>=3 && user_input<=6
+			puts "Not Weird"
+		end
+		if user_input>=7 && user_input<=20
+			puts "Weird"
+		end
+		if user_input>20
+			puts "Not Weird"
+		end
+	end
+
+	# Ask user to continue or not
+	puts "Press Y to continue, other letter to exit"
+	continue = gets.chomp
+	break if continue != 'Y'
+end
